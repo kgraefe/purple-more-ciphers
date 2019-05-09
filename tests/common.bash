e4b2386ddcd6d47569bc51f33c9b560b83343b28
Konrad Gräfe <konradgraefe@aol.com>
2019-05-09 17:33:30 +0200

    tests: Extract common functions
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

---
+++ tests/common.bash
@@ -0,0 +1,19 @@
+CHECK="${CHECK:-./tests/check}"
+
+# The check executable uses libpurple and glib which both need even more
+# libraries. On Windows those are not installed globally so we try to borrow
+# them from an existing Pidgin installation.
+if [[ $OSTYPE == cygwin* ]]; then
+	if [ -z "$PIDGINPATH" ]; then
+		PIDGINPATH="$(
+			cygpath -f /proc/registry32/HKEY_LOCAL_MACHINE/SOFTWARE/pidgin/@
+		)"
+	fi
+	export PATH="$PIDGINPATH:$PIDGINPATH/Gtk/bin:$PATH"
+fi
+
+digest() {
+	echo "$output" | awk 'BEGIN {RS="\r\n|\n"} /test: Digest:/ {$4=$4; printf "%s",$4}'
+}
+
+
